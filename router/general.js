4c85f38f984c314aa43b958be5394d3d7f2aff0e
buponbonbon <khangtran.mschcm@gmail.com>
2026-05-21 20:21:02 +0700

    f

--- router/general.js
+++ router/general.js
@@ -3,50 +3,62 @@ let books = require("./booksdb.js");
 const axios = require('axios');
 const public_users = express.Router();
 
-// Get all books list
-// Input: None | Output: JSON list of all books
+/**
+ * Route: Get all books
+ * Input: None
+ * Output: JSON list of all books
+ */
 public_users.get('/', async function (req, res) {
     try {
         const response = await axios.get('http://localhost:5000/');
-        res.send(JSON.stringify(response.data, null, 4));
+        res.status(200).json(response.data);
     } catch (error) {
-        res.status(500).send("Error fetching books");
+        res.status(500).json({ message: "Error fetching books" });
     }
 });
 
-// Get book details based on ISBN
-// Input: isbn (URL parameter) | Output: JSON object of the specific book
+/**
+ * Route: Get book by ISBN
+ * Input: isbn (URL parameter)
+ * Output: JSON object of the book matching the ISBN
+ */
 public_users.get('/isbn/:isbn', async function (req, res) {
     try {
         const isbn = req.params.isbn;
         const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
-        res.send(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
-        res.status(404).send("Book not found");
+        res.status(404).json({ message: `Book with ISBN ${req.params.isbn} not found` });
     }
 });
 
-// Get book details based on author
-// Input: author (URL parameter) | Output: JSON list of books by the author
+/**
+ * Route: Get books by Author
+ * Input: author (URL parameter)
+ * Output: JSON list of books by the specified author
+ */
 public_users.get('/author/:author', async function (req, res) {
     try {
         const author = req.params.author;
         const response = await axios.get(`http://localhost:5000/author/${encodeURIComponent(author)}`);
-        res.send(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
-        res.status(404).send("No books found for this author");
+        res.status(404).json({ message: `No books found for author: ${req.params.author}` });
     }
 });
 
-// Get all books based on title
-// Input: title (URL parameter) | Output: JSON list of books matching the title
+/**
+ * Route: Get books by Title
+ * Input: title (URL parameter)
+ * Output: JSON list of books matching the specified title
+ */
 public_users.get('/title/:title', async function (req, res) {
     try {
         const title = req.params.title;
         const response = await axios.get(`http://localhost:5000/title/${encodeURIComponent(title)}`);
-        res.send(response.data);
+        res.status(200).json(response.data);
     } catch (error) {
-        res.status(404).send("No books found with this title");
+        res.status(404).json({ message: `No books found with title: ${req.params.title}` });
     }
 });
 
