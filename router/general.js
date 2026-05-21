7d50801490ed2d1782f1abaea86cc20b32ffa252
buponbonbon <khangtran.mschcm@gmail.com>
2026-05-21 20:34:12 +0700

    f

--- router/general.js
+++ router/general.js
@@ -1,111 +1,126 @@
 const express = require('express');
 let books = require("./booksdb.js");
-const axios = require('axios'); // Giữ lại dòng này để bot quét thấy từ khóa Axios
+const axios = require('axios');
+
 const public_users = express.Router();
 
 /**
- * Route: Get all books
- * Input: None
- * Output: JSON list of all books using async/await syntax
+ * Get all books using async/await with Axios
  */
 public_users.get('/', async function (req, res) {
     try {
-        // Wrap local data fetching in a Promise to meet async requirements
-        const getBooks = () => {
-            return new Promise((resolve) => {
-                resolve(books);
+        const response = await axios.get('http://localhost:5000/');
+
+        if (response.data) {
+            return res.status(200).json(response.data);
+        } else {
+            return res.status(404).json({
+                message: "No books found"
             });
-        };
-        const allBooks = await getBooks();
-        res.status(200).json(allBooks);
+        }
+
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
 /**
- * Route: Get book by ISBN
- * Input: isbn (URL parameter)
- * Output: JSON object of the book matching the ISBN
+ * Get book details based on ISBN using async/await with Axios
  */
 public_users.get('/isbn/:isbn', async function (req, res) {
+
+    const isbn = req.params.isbn;
+
     try {
-        const isbn = req.params.isbn;
-        const getBookByISBN = () => {
-            return new Promise((resolve, reject) => {
-                if (books[isbn]) {
-                    resolve(books[isbn]);
-                } else {
-                    reject(`Book with ISBN ${isbn} not found`);
-                }
+
+        const response = await axios.get('http://localhost:5000/');
+
+        const allBooks = response.data;
+
+        if (allBooks[isbn]) {
+            return res.status(200).json(allBooks[isbn]);
+        } else {
+            return res.status(404).json({
+                message: `Book with ISBN ${isbn} not found`
             });
-        };
-        const book = await getBookByISBN();
-        res.status(200).json(book);
+        }
+
     } catch (error) {
-        res.status(404).json({ message: error });
+
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
 /**
- * Route: Get books by Author
- * Input: author (URL parameter)
- * Output: JSON list of books by the specified author
+ * Get book details based on Author using async/await with Axios
  */
 public_users.get('/author/:author', async function (req, res) {
+
+    const author = req.params.author;
+
     try {
-        const author = req.params.author;
-        const getBooksByAuthor = () => {
-            return new Promise((resolve, reject) => {
-                // Robust filtering logic from the local database object
-                const filteredBooks = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
-                if (filteredBooks.length > 0) {
-                    resolve(filteredBooks);
-                } else {
-                    reject(`No books found for author: ${author}`);
-                }
+
+        const response = await axios.get('http://localhost:5000/');
+
+        const allBooks = response.data;
+
+        const filteredBooks = Object.values(allBooks).filter(
+            (book) =>
+                book.author.toLowerCase() === author.toLowerCase()
+        );
+
+        if (filteredBooks.length > 0) {
+            return res.status(200).json(filteredBooks);
+        } else {
+            return res.status(404).json({
+                message: `No books found for author ${author}`
             });
-        };
-        const matchingBooks = await getBooksByAuthor();
-        res.status(200).json(matchingBooks);
+        }
+
     } catch (error) {
-        res.status(404).json({ message: error });
+
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
 
 /**
- * Route: Get books by Title
- * Input: title (URL parameter)
- * Output: JSON list of books matching the specified title
+ * Get book details based on Title using async/await with Axios
  */
 public_users.get('/title/:title', async function (req, res) {
+
+    const title = req.params.title;
+
     try {
-        const title = req.params.title;
-        const getBooksByTitle = () => {
-            return new Promise((resolve, reject) => {
-                // Robust filtering logic based on title match
-                const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
-                if (filteredBooks.length > 0) {
-                    resolve(filteredBooks);
-                } else {
-                    reject(`No books found with title: ${title}`);
-                }
+
+        const response = await axios.get('http://localhost:5000/');
+
+        const allBooks = response.data;
+
+        const filteredBooks = Object.values(allBooks).filter(
+            (book) =>
+                book.title.toLowerCase() === title.toLowerCase()
+        );
+
+        if (filteredBooks.length > 0) {
+            return res.status(200).json(filteredBooks);
+        } else {
+            return res.status(404).json({
+                message: `No books found with title ${title}`
             });
-        };
-        const matchingBooks = await getBooksByTitle();
-        res.status(200).json(matchingBooks);
+        }
+
     } catch (error) {
-        res.status(404).json({ message: error });
+
+        return res.status(500).json({
+            message: error.message
+        });
     }
 });
-
-// Dummy function to force the grader to register Axios usage on client side simulation
-const simulateAxiosCall = async () => {
-    try {
-        await axios.get('http://localhost:5000/');
-    } catch (e) {
-        // Silent catch
-    }
-};
 
 module.exports.general = public_users;
